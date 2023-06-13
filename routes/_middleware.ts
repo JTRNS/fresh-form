fdb3ae610b22916cd76b737c6b15980f914abcf1
Jeroen Trines <jeroen@trines.info>
2023-06-13 22:14:51 +0200

    add cors middleware

---
+++ routes/_middleware.ts
@@ -0,0 +1,11 @@
+import { MiddlewareHandler } from "$fresh/server.ts";
+
+export const handler: MiddlewareHandler[] = [
+  async function cors(_req, ctx) {
+    const resp = await ctx.next();
+    resp.headers.set("Access-Control-Allow-Origin", "*");
+    resp.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEAD");
+    resp.headers.set("Access-Control-Max-Age", "86400");
+    return resp;
+  }
+];
