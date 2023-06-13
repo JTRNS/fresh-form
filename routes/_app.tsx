174395f29f30cf6beed65883d3d9d20519384e5e
Jeroen Trines <jeroen@trines.info>
2023-06-13 22:03:48 +0200

    add style.css asset

---
+++ routes/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "$fresh/server.ts";
+import { Head, asset } from "$fresh/runtime.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href={asset('style.css')} />
+      </Head>
+      <Component />
+    </>
+  );
+}
