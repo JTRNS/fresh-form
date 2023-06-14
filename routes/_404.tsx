5b4bcb1d7e6f0f27b75366c9dbd31bf9349029aa
Jeroen Trines <jeroen@trines.info>
2023-06-14 08:25:04 +0200

    add custom 404 page

---
+++ routes/_404.tsx
@@ -0,0 +1,18 @@
+import { UnknownPageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
+
+export default function NotFoundPage({ url }: UnknownPageProps) {
+  return (
+    <main class="not-found">
+      <h1>404</h1>
+      <p>not found</p>
+      <picture>
+        <img src={asset('dino_404.png')} alt="Dinosaur detective looking for webpage with spyglass." />
+      </picture>
+
+      <code>{url.href}</code>
+      <p>We have looked everywhere, but the page seems to be missing.</p>
+      <a href="/">return to home</a>
+    </main>
+  );
+}
