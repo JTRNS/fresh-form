870b8d1c027a56dd5ed31ba395b355e6e30eb7ff
Jeroen Trines <jeroen@trines.info>
2023-06-14 09:07:56 +0200

    add PATCH to allowed methods

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -4,7 +4,7 @@ export const handler: MiddlewareHandler[] = [
   async function cors(_req, ctx) {
     const resp = await ctx.next();
     resp.headers.set("Access-Control-Allow-Origin", "*");
-    resp.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEAD");
+    resp.headers.set("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, HEAD");
     resp.headers.set("Access-Control-Max-Age", "86400");
     return resp;
   }
