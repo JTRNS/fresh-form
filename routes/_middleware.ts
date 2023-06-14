0317d6288825c1a31a90466ded3a5bfa05a71d5a
Jeroen Trines <jeroen@trines.info>
2023-06-14 10:23:03 +0200

    add http error handling middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,4 +1,6 @@
 import { MiddlewareHandler } from "$fresh/server.ts";
+import { isHttpError } from "$std/http/http_errors.ts";
+
 
 export const handler: MiddlewareHandler[] = [
   async function cors(_req, ctx) {
@@ -7,5 +9,17 @@ export const handler: MiddlewareHandler[] = [
     resp.headers.set("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, HEAD");
     resp.headers.set("Access-Control-Max-Age", "86400");
     return resp;
+  },
+  async function error(_req, ctx) {
+    try {
+      return await ctx.next();
+    } catch (e) {
+      if (isHttpError(e)) {
+        const response = new Response(e.message, { status: e.status });
+        return response;
+      } else {
+        return new Response("Internal Server Error", { status: 500 });
+      }
+    }
   }
 ];
